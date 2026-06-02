d10652fc011c81844f24cb14ac7b2e6bb71398cc
Aluoye <naomiemuekpere99@gmail.com>
2026-06-02 23:27:07 +0100

    updated footer.jsx

--- component/footer.jsx
+++ component/footer.jsx
@@ -1,6 +1,4 @@
 
-const Footer = () => {
-  return (
     <footer className="site-footer">
       <div className="footer-about">
         <h3>About</h3>
@@ -51,42 +49,19 @@ const Footer = () => {
 
       <div className="footer-bottom">
         <p>
-          &copy;2026 Design by{" "}
-          <a href="https://amakandukwu.com/" target="_blank" rel="noreferrer">
-            Amaka
-          </a>{" "}
-          &amp;{" "}
-          <a
-            href="https://www.linkedin.com/in/ifeomaokocha"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Ifeoma A
-          </a>
-          .
-          <br />
-          Built by{" "}
+          &copy;2026 Design by Group 15
+          Built by
           <a
             href="https://github.com/Erijesu0/Group-15-Capstone-project-phoenix.git"
             target="_blank"
-            rel="noreferrer"
-          >
-            Phoenix Cohort: Group 15
+            rel="noreferrer"> Phoenix Cohort: Group 15
           </a>
           . All rights reserved
         </p>
 
-        <a
-          href="https://tsacademyonline.com/"
-          target="_blank"
-          rel="noreferrer"
-          className="footer-ts-link"
-        >
-          TSAcademy
-        </a>
+        <div class="footer-right">
+        <p>TSAcademy</p>
+        </div>
+        
       </div>
     </footer>
-  );
-};
-
-export default Footer;
