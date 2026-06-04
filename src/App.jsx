24c593f231b6c094409918cd5ddf6f192f792365
Toluwani Erijesu <toluwanierijesu@gmail.com>
2026-06-04 14:03:38 +0100

    updated main

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./Components/Hero";
 import PlanetGrid from "./Components/PlanetGrid";
 import ContactForm from "./Components/ContactForm";
 import VideoSection from "./Components/VideoSection";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Hero />
         </div>
       </div>
+
       <div className="bg-white">
         <div className="container">
           <VideoSection />
@@ -25,8 +27,16 @@ function App() {
         </div>
       </div>
 
-      <div className="container">
-        <ContactForm />
+      <div className="bg-white">
+        <div className="container">
+          <ContactForm />
+        </div>
+      </div>
+
+      <div className="bg-dark">
+        <div className="container footer-container">
+          <Footer />
+        </div>
       </div>
     </div>
   );
